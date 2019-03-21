a153fb0d98bef47111a77c17b1a339bd60076b2b
JavsShao <javs_shao@163.com>
2019-03-21 15:06:36 +0800

    项目基本配置_Config类_加载配置

--- manage.py
+++ manage.py
@@ -3,6 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+
+app.config.from_object(Config)
+
 @app.route('/index')
 def index():
     return 'index'
