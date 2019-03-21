a22ef59ca82a0bd96fad457ce9a60278d9a3b809
JavsShao <javs_shao@163.com>
2019-03-21 15:17:43 +0800

    项目基本配置_Config类_SQLAlchemy

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -6,8 +7,12 @@ app = Flask(__name__)
 class Config(object):
     """工程配置信息"""
     DEBUG = True
+    # 数据库配置信息_MySQL
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 @app.route('/index')
 def index():
