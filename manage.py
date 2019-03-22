f9bfdf498f64feb5f7183afcc4f0bcdf24e94b8c
JavsShao <javs_shao@163.com>
2019-03-22 10:20:09 +0800

    代码抽取_抽取配置文件

--- manage.py
+++ manage.py
@@ -7,26 +7,12 @@ from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+from config import Config
+
 
 app = Flask(__name__)
 
-class Config(object):
-    """工程配置信息"""
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-    DEBUG = True
-    # 数据库配置信息_MySQL
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # 数据库_Redis配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"  # 指定session保存到redis中
-    SESSION_USE_SIGNER = True   # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
-
+# 配置
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
