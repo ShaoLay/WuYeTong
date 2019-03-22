15adbd182b9b02c9931743518f4500ea1b9f3a36
JavsShao <javs_shao@163.com>
2019-03-22 10:12:01 +0800

    项目基本配置_Config类_Flask-Script与数据库迁移配置

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = Flask(__name__)
@@ -30,10 +32,13 @@ db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 Session(app)
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @app.route('/index')
 def index():
     return 'index'
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
