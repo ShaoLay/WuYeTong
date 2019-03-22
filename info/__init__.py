228d1f66d4a4ba04bb738e18045054ee899a0641
JavsShao <javs_shao@163.com>
2019-03-22 10:31:02 +0800

    代码抽取_业务逻辑独立

---
+++ info/__init__.py
@@ -0,0 +1,18 @@
+import redis
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
+
+from config import Config
+
+
+app = Flask(__name__)
+
+# 配置
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+CSRFProtect(app)
+Session(app)
--- manage.py
+++ manage.py
@@ -1,23 +1,8 @@
-import redis
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
-from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from info import *
 
-from config import Config
 
-
-app = Flask(__name__)
-
-# 配置
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-CSRFProtect(app)
-Session(app)
 manager = Manager(app)
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
