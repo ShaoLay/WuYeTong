a8b9536a00a8556612a8ac3765396f44cbe8c574
JavsShao <javs_shao@163.com>
2019-03-22 10:54:50 +0800

    代码抽取_日志配置_1

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import logging
+
 import redis
 
 
@@ -21,13 +23,18 @@ class Config(object):
     SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
     PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
 
+    # 默认日志等级
+    LOG_LEVEL = logging.DEBUG
+
+
 class DevelopementConfig(Config):
     """开发模式下的配置"""
     DEBUG = True
 
 class ProductionConfig(Config):
     """生产模式下的配置"""
-    pass
+    """生产模式下的配置"""
+    LOG_LEVEL = logging.ERROR
 
 
 # 定义配置字典
--- info/__init__.py
+++ info/__init__.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 
-import config
 from config import Config
 
 
