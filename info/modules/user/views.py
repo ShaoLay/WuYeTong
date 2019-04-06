3cbbf2d0accb5c5c91823b5f85f23d99a0582d32
JavsShao <javs_shao@163.com>
2019-04-06 16:56:12 +0800

    用户个人中心_用户新闻收藏之前后端口实现

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -6,6 +6,50 @@ from info.utils.comment import user_login_data
 from info import response_code, db, constants
 
 
+@user_blue.route('/user_collection')
+@user_login_data
+def user_collection():
+    """用户收藏"""
+    # 1.获取登录用户信息
+    user = g.user
+    if not user:
+        return redirect(url_for('index.index'))
+
+        # 2.接受参数
+        page = request.args.get('p', '1')
+
+        # 3.校验参数
+        try:
+            page = int(page)
+        except Exception as e:
+            current_app.logger.error(e)
+            page = '1'  # 公司中对于这种处理会有专门的方案的，不需要操心
+
+        # 4.分页查询 : user.collection_news == BaseQuery类型的对象
+        paginate = None
+        try:
+            paginate = user.collection_news.paginate(page, constants.USER_COLLECTION_MAX_NEWS, False)
+        except Exception as e:
+            current_app.logger.error(e)
+
+        # 5.构造渲染模板的数据
+        news_list = paginate.items
+        total_page = paginate.pages
+        current_page = paginate.page
+
+        news_dict_list = []
+        for news in news_list:
+            news_dict_list.append(news.to_basic_dict())
+
+        context = {
+            'news_list': news_dict_list,
+            'total_page': total_page,
+            'current_page': current_page
+        }
+
+        # 6.渲染模板
+        return render_template('news/user_collection.html', context=context)
+
 @user_blue.route('/pass_info', methods=['GET', 'POST'])
 @user_login_data
 def pass_info():
